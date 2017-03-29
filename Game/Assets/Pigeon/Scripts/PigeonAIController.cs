f5e71331f9ebc678223eece204ce62901756aa1f
E-vanderHeide <ewoud.vanderheide@outlook.com>
2017-03-29 12:16:10 +0200

    commented and removed parts of the code

--- Game/Assets/Pigeon/Scripts/PigeonAIController.cs
+++ Game/Assets/Pigeon/Scripts/PigeonAIController.cs
@@ -7,108 +7,115 @@ using System;
 public class PigeonAIController : MonoBehaviour {
 	public PigeonCharacterScript3 pigeonCharacter;
 
+    //Ai states
     enum PigeonState
     {
-        eating,
-        walking,
-        seekingfood,
-        scared
+       //TODO: make states here
     }
 
+    //current state of the AI
     PigeonState currentState;
+
+    //Time the player has been eating
     DateTime eatTime = DateTime.Now;
     List<GameObject> foodList;
+    //radius in which the pigeon gets scared. 
     public float scareRadius;
 
     void Start () {
 		pigeonCharacter = GetComponent<PigeonCharacterScript3> ();
-        currentState = PigeonState.eating;
+
+        //initalize the eat time.
         eatTime = DateTime.Now;
-       foodList = new List<GameObject>(GameObject.FindGameObjectsWithTag("Food"));
-        
+
+        //Find all the food on the map
+        foodList = new List<GameObject>(GameObject.FindGameObjectsWithTag("Food"));
+        //Select a random food item.
         pigeonCharacter.food = foodList[UnityEngine.Random.Range(0, foodList.Count)];
-	}
-
-	void SeekFood(){
-		Vector3 targetRelPos = pigeonCharacter.food.transform.position - transform.position;
-        targetRelPos.y = 0;
-        //Vector3 targetRelHzPos = pigeonCharacter.food.transform.position - transform.position; //new Vector3(targetRelPos.x, 0, targetRelPos.z);
-		if (targetRelPos.sqrMagnitude < 10f && pigeonCharacter.isFlying && !pigeonCharacter.soaring) {
-            
-			pigeonCharacter.tryingToLand = true;
-			pigeonCharacter.SetForwardAcceleration (-1.5f);
-			pigeonCharacter.SetUpSpeed (-1.5f);
-		} else if (targetRelPos.sqrMagnitude > 30f && !pigeonCharacter.isFlying && !pigeonCharacter.tryingToLand) {
-			pigeonCharacter.Soar ();
-		} else if (targetRelPos.sqrMagnitude < .083f) {
-			pigeonCharacter.SetForwardAcceleration(0f);
-			pigeonCharacter.Eat();
-
-
-            eatTime = DateTime.Now.AddSeconds(UnityEngine.Random.Range(10, 60));
-            currentState = PigeonState.eating;
-
-		}else{
-            //if(pigeonCharacter.transform.position.y >1)
-            //{
-            //    pigeonCharacter.upSpeed = 0;
-            //}
-           
-            pigeonCharacter.SetForwardAcceleration(.5f);
-        }
-        targetRelPos.Normalize();
-		pigeonCharacter.turnSpeed = -Vector3.Dot (targetRelPos, transform.forward);
-	}
-
-    private void ResolveState()
-    {
-        //Debug.Log(string.Format("Current Pigeon state = {0}", currentState));
-        switch(currentState)
-        {
-            case PigeonState.eating:
-                if(DateTime.Now > eatTime)
-                {
-                    pigeonCharacter.food = foodList[UnityEngine.Random.Range(0, foodList.Count)];
-                    currentState = PigeonState.seekingfood; 
-                }
-                break;
-            case PigeonState.seekingfood:
-                SeekFood();
-                break;
-            case PigeonState.scared:
-                pigeonCharacter.Soar();
-                break;
-            default:
-                break;
-        }
+        //Set initial state.
+        //TODO: Initial state
     }
 
+    /// <summary>
+    /// Update loop, called every frame.
+    /// </summary>
+    void FixedUpdate()
+    {
+        //TODO: place something here
+    }
+
+    /// <summary>
+    /// Checks if player is close and sets state accordingly
+    /// </summary>
     private void CheckForPlayer()
     {
-        bool playerFound = false;
+      
+    }
+
+    /// <summary>
+    /// decide what to do based on state
+    /// </summary>
+    private void ResolveState()
+    {
+        
+    }
+
+    void SeekFood()
+    {
+        //Find relative position of target.
+        Vector3 targetRelPos = pigeonCharacter.food.transform.position - transform.position;
+        //only regard 2d distance, not height.
+        targetRelPos.y = 0;
+        //If the pigeon is flying and the target is close, start landing.
+        if (targetRelPos.sqrMagnitude < 10f && pigeonCharacter.isFlying && !pigeonCharacter.soaring)
+        {
+
+            pigeonCharacter.tryingToLand = true;
+            pigeonCharacter.SetForwardAcceleration(-1.5f);
+            pigeonCharacter.SetUpSpeed(-1.5f);
+        }
+        //If the pigeon is not flying and the target is far away, soar.
+        else if (targetRelPos.sqrMagnitude > 30f && !pigeonCharacter.isFlying && !pigeonCharacter.tryingToLand)
+        {
+            pigeonCharacter.Soar();
+
+        }
+        //if the target is really close, start eating.
+        else if (targetRelPos.sqrMagnitude < .083f)
+        {
+            pigeonCharacter.SetForwardAcceleration(0f);
+            pigeonCharacter.Eat();
+
+            eatTime = DateTime.Now.AddSeconds(UnityEngine.Random.Range(10, 60));
+            //TODO: set state here
+
+        }
+        //Accelerate towards the target.
+        else
+        {
+            pigeonCharacter.SetForwardAcceleration(.5f);
+        }
+        //Face the target
+        targetRelPos.Normalize();
+        pigeonCharacter.turnSpeed = -Vector3.Dot(targetRelPos, transform.forward);
+    }
+
+    /// <summary>
+    /// Check if there is a player close to the pigeon
+    /// </summary>
+    /// <returns></returns>
+    private bool playerClose()
+    {
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, scareRadius);
         {
-            foreach(Collider collider in hitColliders)
+            foreach (Collider collider in hitColliders)
             {
-                if (collider.gameObject.GetComponent<ThirdPersonCharacter>()){
-                    playerFound = true;
-                    
+                if (collider.gameObject.GetComponent<ThirdPersonCharacter>())
+                {
+                    return true;
                 }
             }
         }
-        if(playerFound)
-        {
-            currentState = PigeonState.scared;
-        }
-        else if(currentState == PigeonState.scared)
-        {
-            pigeonCharacter.food = foodList[UnityEngine.Random.Range(0, foodList.Count)];
-            currentState = PigeonState.seekingfood;
-        }
+        return false;
     }
-
-	void FixedUpdate(){
-        CheckForPlayer();
-        ResolveState();
-	}
 }
